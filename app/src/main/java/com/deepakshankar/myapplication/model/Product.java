ec37ea2d1aa459fb07b523960eaecc07ee6f80a7
Deepak <drs3114@rit.edu>
2017-01-25 22:31:36 -0500

    initial commit

---
+++ app/src/main/java/com/deepakshankar/myapplication/model/Product.java
@@ -0,0 +1,13 @@
+package com.deepakshankar.myapplication.model;
+
+/**
+ * This is a class used to store the products returned from the Zappos API.
+ * Created by DeepakShankar on 1/25/2017.
+ * @version 1.0
+ */
+
+public class Product {
+
+    private String name;
+    //private
+}
