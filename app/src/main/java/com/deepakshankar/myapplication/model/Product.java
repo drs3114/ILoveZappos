8669241b59b9b4255abba17a9036beaa9f515c77
Deepak <drs3114@rit.edu>
2017-01-26 10:41:10 -0500

    gradle file updated to fetch the Retrofit dependency. Products class updated to fit the Zappos REST API results.

--- app/src/main/java/com/deepakshankar/myapplication/model/Product.java
+++ app/src/main/java/com/deepakshankar/myapplication/model/Product.java
@@ -2,12 +2,174 @@ package com.deepakshankar.myapplication.model;
 
 /**
  * This is a class used to store the products returned from the Zappos API.
- * Created by DeepakShankar on 1/25/2017.
+ * Created by Deepak Shankar on 1/25/2017.
+ *
+ * @author Deepak Shankar
  * @version 1.0
  */
 
 public class Product {
 
-    private String name;
-    //private
+    private String brandName;
+    private String thumbnailImageUrl;
+    private int productId;
+    private String originalPrice;
+    private int styleId;
+    private int colorId;
+    private String price;
+    private String percentOff;
+    private String productUrl;
+    private String productName;
+
+    /**
+     *
+     * @return
+     */
+    public String getBrandName() {
+        return brandName;
+    }
+
+    /**
+     *
+     * @param brandName
+     */
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getThumbnailImageUrl() {
+        return thumbnailImageUrl;
+    }
+
+    /**
+     *
+     * @param thumbnailImageUrl
+     */
+    public void setThumbnailImageUrl(String thumbnailImageUrl) {
+        this.thumbnailImageUrl = thumbnailImageUrl;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getProductId() {
+        return productId;
+    }
+
+    /**
+     *
+     * @param productId
+     */
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getOriginalPrice() {
+        return originalPrice;
+    }
+
+    /**
+     *
+     * @param originalPrice
+     */
+    public void setOriginalPrice(String originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getStyleId() {
+        return styleId;
+    }
+
+    /**
+     *
+     * @param styleId
+     */
+    public void setStyleId(int styleId) {
+        this.styleId = styleId;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getColorId() {
+        return colorId;
+    }
+
+    /**
+     *
+     * @param colorId
+     */
+    public void setColorId(int colorId) {
+        this.colorId = colorId;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getPrice() {
+        return price;
+    }
+
+    /**
+     *
+     * @param price
+     */
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getPercentOff() {
+        return percentOff;
+    }
+
+    public void setPercentOff(String percentOff) {
+        this.percentOff = percentOff;
+    }
+
+    public String getProductUrl() {
+        return productUrl;
+    }
+
+    /**
+     *
+     * @param productUrl
+     */
+    public void setProductUrl(String productUrl) {
+        this.productUrl = productUrl;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getProductName() {
+        return productName;
+    }
+
+    /**
+     *
+     * @param productName
+     */
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
 }
